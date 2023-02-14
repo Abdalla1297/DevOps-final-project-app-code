744cab3fefee01a3cee3613b103d081bb2e83847
Abdalla1297 <abdalla.Ahmed1297@hotmail.com>
2023-02-14 23:14:47 +0200

    add vm to use ansibal

---
+++ terraform/vm_instance.tf
@@ -0,0 +1,31 @@
+resource "google_compute_instance" "private_vm" {
+  name         = "private_vm"
+  machine_type = "e2-micro"
+  zone         = "us-east4-b"
+  allow_stopping_for_update = true
+
+  depends_on = [
+    google_container_cluster.private-cluster
+   , google_container_node_pool.nodepool
+  ]
+  metadata_startup_script = "${file("./setp.sh")}"
+
+  boot_disk {
+    initialize_params {
+      image = "ubuntu-os-cloud/ubuntu-2204-lts"
+      size = 60
+    
+    }
+  }
+ 
+  network_interface {
+    network = module.network.vpc_name_output
+    subnetwork = module.network.subnet_1_name_output
+   
+  }
+  service_account {
+        email = google_service_account.project-service-account.email
+        scopes = [ "cloud-platform" ]
+}
+
+}
