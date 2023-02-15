33dc577b7923bb9946461ac17197f0602e4225c7
Abdalla1297 <abdalla.Ahmed1297@hotmail.com>
2023-02-15 02:15:33 +0200

    edit pathes

--- terraform/setp.sh
+++ terraform/setp.sh
@@ -1,25 +1,16 @@
 #!/bin/bash
-#To install Google Cli
+# Installing Google Cli
 sudo apt install -y apt-transport-https ca-certificates gnupg
 echo "deb [signed-by=/usr/share/keyrings/cloud.google.gpg] https://packages.cloud.google.com/apt cloud-sdk main" | sudo tee -a /etc/apt/sources.list.d/google-cloud-sdk.list
 curl https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo tee /usr/share/keyrings/cloud.google.gpg
 sudo apt-get update && sudo apt-get install -y google-cloud-cli
-#To Install Kubectl
+# Installing Kubectl
 sudo apt-get install kubectl -y
-#To Install google plugin
+# Installing google plugin
 sudo apt-get install google-cloud-sdk-gke-gcloud-auth-plugin -y
 #Git Clone
-sudo git clone https://github.com/Abdalla1297/DevOps-final-project-terraform.git /home/abdalla/files
+sudo git clone https://github.com/Abdalla1297/DevOps-final-project-terraform.git /home/abdalla/myfile
 #To install ansible 
 sudo apt-get install -y ansible
 #Run Ansible Playbook To Connect With cluster And deploy Jenkins With Services
 sudo ansible-playbook /home/mohamed/files/ansible.yaml
-#To install docker
-sudo apt update -y
-sudo apt-get install apt-transport-https ca-certificates curl software-properties-common -y
-sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu  $(lsb_release -cs)  stable" -y
-sudo apt update -y
-sudo apt-get install docker-ce -y
-sudo systemctl start docker
-sudo systemctl enable docker
